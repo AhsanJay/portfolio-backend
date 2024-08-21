fbd18b5c742593257d9de236b2192e79ed9de36a
Hasan Raza <95583135+AhsanJay@users.noreply.github.com>
2024-08-21 05:51:27 +0500

    Initial commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
 app.use(cors({ origin: '*' })); 
